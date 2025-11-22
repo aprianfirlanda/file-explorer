e3c61eac37a61d56c61f86ed5a14bded639fe1e7
aprianfirlanda <aprianfirlanda@gmail.com>
2025-11-22 10:22:12 +0700

    feat(setup project): group api using prefix

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,10 +1,11 @@
 import {Elysia} from 'elysia';
 import {folderRouter} from "./modules/folder/folder.router.ts";
 
-const app = new Elysia();
+const apiV1Routes = new Elysia({prefix: "/api/v1"})
+  .use(folderRouter);
 
-folderRouter(app);
-
-app.listen(3000);
+const app = new Elysia()
+  .use(apiV1Routes)
+  .listen(3000)
 
 console.log(`Elysia is running at http://localhost:${app.server?.port}`);
--- apps/backend/src/modules/folder/folder.router.ts
+++ apps/backend/src/modules/folder/folder.router.ts
@@ -1,16 +1,16 @@
-import type { Elysia } from "elysia";
-import { folderService } from "./folder.service";
+import {folderService} from "./folder.service";
+import {Elysia} from "elysia";
 
-export const folderRouter = (app: Elysia) =>
-  app
+export const folderRouter = () =>
+  new Elysia({prefix: "/folders"})
     // 1. Full folder tree (for left panel)
-    .get("/folders/tree", async () => {
+    .get("/tree", async () => {
       const data = await folderService.getFolderTree();
       return { data };
     })
 
     // 2. Direct children of a folder (for right panel)
-    .get("/folders/:id/children", async ({ params }) => {
+    .get("/:id/children", async ({params}) => {
       const data = await folderService.getChildren(params.id);
       return { data };
     })
